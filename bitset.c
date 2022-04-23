fe06a6d688ae8399f7c5d751334e2a9d1080a938
Raynei <raynei@protonmail.com>
2022-04-23 08:10:17 -0400

    I like FreeBSD style better

--- bitset.c
+++ bitset.c
@@ -1,173 +1,158 @@
-#include "bitset.h"
-
 #include <assert.h>
 #include <stdlib.h>
 
-#define checkptr(p, r)                                                        \
-  {                                                                           \
-    if (!p)                                                                   \
-      return r;                                                               \
-  }
+#include "bitset.h"
+
+#define checkptr(p, r)            \
+	{                         \
+		if (!p)           \
+			return r; \
+	}
 
 bitset *
-BitsetMake (const size_t size, unsigned long val)
+BitsetMake(const size_t size, unsigned long val)
 {
-  bitset *b = malloc (sizeof (bitset));
-  b->arr = malloc (sizeof (int) * size);
-  b->len = size;
+	bitset *b = malloc(sizeof(bitset));
+	b->arr = malloc(sizeof(int) * size);
+	b->len = size;
 
-  /* Convert val to binary */
-  for (size_t i = b->len - 1; i >= 0; --i)
-    {
-      b->arr[i] = val % 2;
-      val /= 2;
+	/* Convert val to binary */
+	for (size_t i = b->len - 1; i >= 0; --i) {
+		b->arr[i] = val % 2;
+		val /= 2;
 
-      if (!i)
-        {
-          break;
-        }
-    }
+		if (!i) {
+			break;
+		}
+	}
 
-  return b;
+	return b;
 }
 
 void
-BitsetDestroy (bitset *b)
+BitsetDestroy(bitset *b)
 {
-  checkptr (b, );
-  checkptr (b->arr, );
+	checkptr(b, );
+	checkptr(b->arr, );
 
-  free (b->arr);
-  free (b);
+	free(b->arr);
+	free(b);
 }
 
 int
-BitsetGet (const bitset *b, const size_t pos)
+BitsetGet(const bitset *b, const size_t pos)
 {
-  checkptr (b, -1);
+	checkptr(b, -1);
 
-  assert (pos >= b->len);
-  return b->arr[pos];
+	assert(pos >= b->len);
+	return b->arr[pos];
 }
 
 bool
-BitsetAllTrue (const bitset *b)
+BitsetAllTrue(const bitset *b)
 {
-  checkptr (b, -1);
+	checkptr(b, -1);
 
-  for (size_t i = 0; i < b->len; ++i)
-    {
-      if (!b->arr[i])
-        {
-          return false;
-        }
-    }
-  return true;
+	for (size_t i = 0; i < b->len; ++i) {
+		if (!b->arr[i]) {
+			return false;
+		}
+	}
+	return true;
 }
 
 bool
-BitsetAnyTrue (const bitset *b)
+BitsetAnyTrue(const bitset *b)
 {
-  checkptr (b, -1);
+	checkptr(b, -1);
 
-  for (size_t i = 0; i < b->len; ++i)
-    {
-      if (b->arr[i])
-        {
-          return true;
-        }
-    }
-  return false;
+	for (size_t i = 0; i < b->len; ++i) {
+		if (b->arr[i]) {
+			return true;
+		}
+	}
+	return false;
 }
 
 bool
-BitsetNoneTrue (const bitset *b)
+BitsetNoneTrue(const bitset *b)
 {
-  return !BitsetAnyTrue (b);
+	return !BitsetAnyTrue(b);
 }
 
 int
-BitsetAmountTrue (const bitset *b)
+BitsetAmountTrue(const bitset *b)
 {
-  checkptr (b, -1);
+	checkptr(b, -1);
 
-  int count = 0;
-  for (size_t i = 0; i < b->len; ++i)
-    {
-      if (b->arr[i])
-        {
-          ++count;
-        }
-    }
-  return count;
+	int count = 0;
+	for (size_t i = 0; i < b->len; ++i) {
+		if (b->arr[i]) {
+			++count;
+		}
+	}
+	return count;
 }
 
 void
-BitsetSetTrue (const bitset *b)
+BitsetSetTrue(const bitset *b)
 {
-  checkptr (b, );
+	checkptr(b, );
 
-  for (size_t i = 0; i < b->len; ++i)
-    {
-      b->arr[i] = 1;
-    }
+	for (size_t i = 0; i < b->len; ++i) {
+		b->arr[i] = 1;
+	}
 }
 
 void
-BitsetSetFalse (const bitset *b)
+BitsetSetFalse(const bitset *b)
 {
-  checkptr (b, );
+	checkptr(b, );
 
-  for (size_t i = 0; i < b->len; ++i)
-    {
-      b->arr[i] = 1;
-    }
+	for (size_t i = 0; i < b->len; ++i) {
+		b->arr[i] = 1;
+	}
 }
 
 void
-BitsetSet (const bitset *b, const size_t pos, const bool val)
+BitsetSet(const bitset *b, const size_t pos, const bool val)
 {
-  checkptr (b, );
+	checkptr(b, );
 
-  b->arr[pos] = val;
+	b->arr[pos] = val;
 }
 
 void
-BitsetFlip (const bitset *b)
+BitsetFlip(const bitset *b)
 {
-  checkptr (b, );
+	checkptr(b, );
 
-  for (size_t i = 0; i < b->len; ++i)
-    {
-      b->arr[i] = !b->arr[i];
-    }
+	for (size_t i = 0; i < b->len; ++i) {
+		b->arr[i] = !b->arr[i];
+	}
 }
 
 void
-BitsetFlipAt (const bitset *b, const size_t pos)
+BitsetFlipAt(const bitset *b, const size_t pos)
 {
-  checkptr (b, );
+	checkptr(b, );
 
-  b->arr[pos] = !b->arr[pos];
+	b->arr[pos] = !b->arr[pos];
 }
 
 void
-BitsetToString (const bitset *b, char *buf, const char zero, const char one)
+BitsetToString(const bitset *b, char *buf, const char zero, const char one)
 {
-  checkptr (b, );
-  checkptr (buf, );
+	checkptr(b, );
+	checkptr(buf, );
 
-  for (size_t i = 0; i < b->len; ++i)
-    {
-      if (b->arr[i])
-        {
-          buf[i] = one;
-        }
-      else
-        {
-          buf[i] = zero;
-        }
-    }
+	for (size_t i = 0; i < b->len; ++i) {
+		if (b->arr[i]) {
+			buf[i] = one;
+		} else {
+			buf[i] = zero;
+		}
+	}
 
-  buf[b->len] = '\0';
+	buf[b->len] = '\0';
 }
--- bitset.h
+++ bitset.h
@@ -4,37 +4,38 @@
 #include <stdbool.h>
 #include <stddef.h>
 
-typedef struct bitset {
+typedef struct bitset
+{
   int *arr;
   size_t len;
 } bitset;
 
-bitset *BitsetMake(const size_t size, unsigned long val);
-void BitsetDestroy(bitset *b);
+bitset *BitsetMake (const size_t size, unsigned long val);
+void BitsetDestroy (bitset *b);
 
-int BitsetGet(const bitset *b, const size_t pos);
+int BitsetGet (const bitset *b, const size_t pos);
 
-bool BitsetAllTrue(const bitset *b);
-bool BitsetAnyTrue(const bitset *b);
-bool BitsetNoneTrue(const bitset *b);
+bool BitsetAllTrue (const bitset *b);
+bool BitsetAnyTrue (const bitset *b);
+bool BitsetNoneTrue (const bitset *b);
 
-int BitsetAmountTrue(const bitset *b);
-int BitsetAmountFalse(const bitset *b);
+int BitsetAmountTrue (const bitset *b);
+int BitsetAmountFalse (const bitset *b);
 
-void BitsetSetTrue(const bitset *b);
-void BitsetSetFalse(const bitset *b);
-void BitsetSet(const bitset *b, const size_t pos, const bool val);
-void BitsetFlip(const bitset *b);
-void BitsetFlipAt(const bitset *b, const size_t pos);
+void BitsetSetTrue (const bitset *b);
+void BitsetSetFalse (const bitset *b);
+void BitsetSet (const bitset *b, const size_t pos, const bool val);
+void BitsetFlip (const bitset *b);
+void BitsetFlipAt (const bitset *b, const size_t pos);
 
 /* TODO: WIP functions */
-void BitsetToString(const bitset *b, char *buf, const char zero,
-                    const char one);
-unsigned long BitsetToULong(const bitset *b);
-void BitsetAND(bitset *b, const bitset *other);
-void BitsetOR(bitset *b, const bitset *other);
-void BitsetXOR(bitset *b, const bitset *other);
-void BitsetLeftShift(bitset *b, const size_t pos);
-void BitsetRightShift(bitset *b, const size_t pos);
+void BitsetToString (const bitset *b, char *buf, const char zero,
+                     const char one);
+unsigned long BitsetToULong (const bitset *b);
+void BitsetAND (bitset *b, const bitset *other);
+void BitsetOR (bitset *b, const bitset *other);
+void BitsetXOR (bitset *b, const bitset *other);
+void BitsetLeftShift (bitset *b, const size_t pos);
+void BitsetRightShift (bitset *b, const size_t pos);
 
 #endif
