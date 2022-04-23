c737c975caf2afd635409c55368a9a474ce159c6
Raynei <raynei@protonmail.com>
2022-04-23 08:18:52 -0400

    Some basic comments

--- bitset.h
+++ bitset.h
@@ -4,38 +4,50 @@
 #include <stdbool.h>
 #include <stddef.h>
 
-typedef struct bitset
-{
-  int *arr;
-  size_t len;
+typedef struct bitset {
+	int *arr;
+	size_t len;
 } bitset;
 
-bitset *BitsetMake (const size_t size, unsigned long val);
-void BitsetDestroy (bitset *b);
+/* Return a bitset of size and value based on val */
+bitset *BitsetMake(const size_t size, unsigned long val);
+/* Free bitset */
+void BitsetDestroy(bitset *b);
 
-int BitsetGet (const bitset *b, const size_t pos);
+/* Get bit at particular position */
+int BitsetGet(const bitset *b, const size_t pos);
 
-bool BitsetAllTrue (const bitset *b);
-bool BitsetAnyTrue (const bitset *b);
-bool BitsetNoneTrue (const bitset *b);
+/* See if bits are:
+ * All are true
+ * Any being true
+ * None are true */
+bool BitsetAllTrue(const bitset *b);
+bool BitsetAnyTrue(const bitset *b);
+bool BitsetNoneTrue(const bitset *b);
 
-int BitsetAmountTrue (const bitset *b);
-int BitsetAmountFalse (const bitset *b);
+/* Return the amount of true or false */
+int BitsetAmountTrue(const bitset *b);
+int BitsetAmountFalse(const bitset *b);
 
-void BitsetSetTrue (const bitset *b);
-void BitsetSetFalse (const bitset *b);
-void BitsetSet (const bitset *b, const size_t pos, const bool val);
-void BitsetFlip (const bitset *b);
-void BitsetFlipAt (const bitset *b, const size_t pos);
+/* Set all to be true/false */
+void BitsetSetTrue(const bitset *b);
+void BitsetSetFalse(const bitset *b);
+/* Set a certain bit at pos to val */
+void BitsetSet(const bitset *b, const size_t pos, const bool val);
+/* Flip all bits or flip at pos */
+void BitsetFlip(const bitset *b);
+void BitsetFlipAt(const bitset *b, const size_t pos);
+
+/* Converts the bitset into string and write into buf */
+void BitsetToString(const bitset *b, char *buf, const char zero,
+    const char one);
 
 /* TODO: WIP functions */
-void BitsetToString (const bitset *b, char *buf, const char zero,
-                     const char one);
-unsigned long BitsetToULong (const bitset *b);
-void BitsetAND (bitset *b, const bitset *other);
-void BitsetOR (bitset *b, const bitset *other);
-void BitsetXOR (bitset *b, const bitset *other);
-void BitsetLeftShift (bitset *b, const size_t pos);
-void BitsetRightShift (bitset *b, const size_t pos);
+unsigned long BitsetToULong(const bitset *b);
+void BitsetAND(bitset *b, const bitset *other);
+void BitsetOR(bitset *b, const bitset *other);
+void BitsetXOR(bitset *b, const bitset *other);
+void BitsetLeftShift(bitset *b, const size_t pos);
+void BitsetRightShift(bitset *b, const size_t pos);
 
 #endif
